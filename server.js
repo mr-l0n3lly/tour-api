56d571576aa5987db0adb6da00846dbcfa40e051
Andrei Pavalachi <andreipatan@live.com>
2021-03-20 21:06:24 +0200

    Added get and post for our api

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dataTemp/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
@@ -13,6 +15,31 @@ app.get('/api/v1/tours', (req, res) => {
 			tours: tours
 		}
 	});
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+	const required = tours.find(item => item.id === Number(req.params.id));
+
+	if (required === undefined) {
+		res.status(404).json({
+			status: 'failure',
+			data: {
+				tour: 'Wrong id specified'
+			}
+		});
+	} else {
+		res.status(200).json({
+			status: 'success',
+			data: {
+				tour: required
+			}
+		});
+	}
+});
+
+app.post('/api/v1/tours', (req, res) => {
+	console.log(req.body);
+	res.status(200).send('Done');
 });
 
 app.listen(3000, () => {
