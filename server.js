ffc14ffa8861348e737ff6fffa5a17a78867dcf5
Andrei Pavalachi <andreipatan@live.com>
2021-03-21 12:01:09 +0200

    Adde a mini change to this warning

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ env.config({ path: './.env' });
 
 // Start linstening
 app.listen(process.env.PORT, () => {
+	// eslint-disable-next-line no-console
 	console.log(`Server lisntening on ${process.env.PORT}`);
 });
